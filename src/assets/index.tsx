abce99a04bc56439e1f4ce6dde0e13f0dd5726d1
PLong2003 <99625263+PLong2003@users.noreply.github.com>
2023-08-12 10:41:43 +0700

    12/08

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -5,3 +5,17 @@ export const LOGO_QUAFINA = 'https://firebasestorage.googleapis.com/v0/b/aquafin
 export const IMAGE_TEXT_WELLCOME = 'https://firebasestorage.googleapis.com/v0/b/aquafina-6961c.appspot.com/o/Image-Text_Wellcome.png?alt=media&token=4a391d75-906c-4b1b-b798-28ba4e2ea386'
 
 export const IMAGE_FOOTER_AUTHEN = 'https://firebasestorage.googleapis.com/v0/b/aquafina-6961c.appspot.com/o/Image-Footer_Authen.png?alt=media&token=7d14c92d-300c-48a3-9fb5-a80bbf491ed1'
+
+export const IMAGE_AO_THUN_W = 'https://firebasestorage.googleapis.com/v0/b/aquafina-6961c.appspot.com/o/Image-Ao_thun-W.png?alt=media&token=a54a9e08-3b0c-4068-b26f-940c7fe55792';
+
+export const IMAGE_AO_PLAYER = 'https://firebasestorage.googleapis.com/v0/b/aquafina-6961c.appspot.com/o/Image-Ao_layer.png?alt=media&token=f9359d21-e092-479d-b787-b438bab1565f';
+
+export const IMAGE_AO_THUN_B = 'https://firebasestorage.googleapis.com/v0/b/aquafina-6961c.appspot.com/o/Image-Ao_thun-B.png?alt=media&token=947d0120-fe71-4773-9a93-f0ec43f81acc';
+
+export const IMAGE_NON_OFF_W = 'https://firebasestorage.googleapis.com/v0/b/aquafina-6961c.appspot.com/o/Image-Non_Offwhite.png?alt=media&token=4e9d656e-ac5b-4cdd-a7b9-f1110d8b3f73';
+
+export const IMAGE_SOCK = 'https://firebasestorage.googleapis.com/v0/b/aquafina-6961c.appspot.com/o/Image-Sock.png?alt=media&token=f8119b4e-3ce4-48f0-b67e-83e6d88a01d4';
+
+export const IMAGE_TUI_TOTE = 'https://firebasestorage.googleapis.com/v0/b/aquafina-6961c.appspot.com/o/Image-Tui_Tote.png?alt=media&token=c8c9ab2c-118a-4203-b9d5-96c3daef6c15';
+
+export const IMAGE_BG_GIFT = 'https://firebasestorage.googleapis.com/v0/b/aquafina-6961c.appspot.com/o/Image-BG_Gift.png?alt=media&token=c7f800d6-d70d-4d0f-a478-20763a82500c'
